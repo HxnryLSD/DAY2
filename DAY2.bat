9b8eaf85a7297d8d2f4418a8533f789be35befb7
HxnryLSD <henry.richter.hl@gmail.com>
2025-05-05 04:01:01 +0200

    something not working lol. idk

--- DAY2.bat
+++ DAY2.bat
@@ -1,173 +1,183 @@
 @echo off
 
-echo.  
-echo.  
-echo       :::::::::      :::   :::   :::  ::::::::  
-echo       :+:    :+:   :+: :+: :+:   :+: :+:    :+: 
-echo       +:+    +:+  +:+   +:+ +:+ +:+        +:+  
-echo       +#+    +:+ +#++:++#++: +#++:       +#+    
-echo       +#+    +#+ +#+     +#+  +#+      +#+      
-echo       #+#    #+# #+#     #+#  #+#     #+#       
-echo       #########  ###     ###  ###    ########## 
-echo.  
-echo.         Network Optimizer and Resetter
-title DAY2 / Welcome User!
-color 0d
-timeout 2 > nul
-color 0f
-echo.  
-echo.  
-echo.  
-
-echo.       List what will happen:
-echo.  
-
-
-echo.    - Depot forwarding is reset
-echo.    - Depot is reset
-echo.    - Control protocol is reset
-echo.    - Echo sequence request is reset
-echo.    - Global is reset
-echo.    - Reset interface
-echo.    - Anycast address is reset
-echo.    - Reset multicast address
-echo.    - Reset unicast address
-echo.    - Neighbor is reset
-echo.    - Path is reset
-echo.    - Potential is reset
-echo.    - Reset prefix policy
-echo.    - Reset proxy neighbor
-echo.    - Reset route
-echo.    - Reset location prefix
-echo.    - Reset subinterface
-echo.    - Reset reactivation pattern
-echo.    - Resolve neighbor is reset
-echo.    - Flushing DNS Cache
-echo.    - Deleting Temp Data
-echo.    - Clearing NetBIOS name cache
-echo.    - Name resolution refresh
-echo.    - Clearing ARP cache
-echo.    - MAC address reconnecting
-
-title DAY2 / Waiting For Permission ...
-echo.     
-echo.     
-echo.       ! Press Any Button To Continue
-
-timeout 999 > nul
-
+:: Check for administrative privileges at the start of the script
 NET SESSION >nul 2>&1
-IF %ERRORLEVEL% EQU 0 (
-
-cls
-title DAY2 / Received Permissions
-echo.     
-echo.     Administrator rights Detected!
-echo.     
-color 0a
-timeout 1 > nul
-color 0f
-
-) ELSE (
-
-cls
-title DAY2 / ERROR NO ADMIN RIGHTS - EXITING
-echo.     
-echo.     
-echo           :::::::::: :::::::::  :::::::::   ::::::::  :::::::::
-echo          :+:        :+:    :+: :+:    :+: :+:    :+: :+:    :+:
-echo         +:+        +:+    +:+ +:+    +:+ +:+    +:+ +:+    +:+
-echo        +#++:++#   +#++:++#:  +#++:++#:  +#+    +:+ +#++:++#:
-echo       +#+        +#+    +#+ +#+    +#+ +#+    +#+ +#+    +#+
-echo      #+#        #+#    #+# #+#    #+# #+#    #+# #+#    #+#
-echo     ########## ###    ### ###    ###  ########  ###    ###
-echo.     
-echo.     
-echo.     No Administrator rights detected.
-echo.     
-echo.     THE PROCESS IS TERMINATED!
-echo.     
-echo.     
-echo.     Press Any Key To Close...
-echo.     
-color c0
-timeout 1 > nul
-color 0c
-timeout 1 > nul
-color c0
-timeout 1 > nul
-color 0c
-
-timeout 10 > nul
-exit
+IF %ERRORLEVEL% NEQ 0 (
+    powershell -Command "Add-Type -AssemblyName PresentationFramework; [System.Windows.MessageBox]::Show('This script requires administrative privileges. Please run it as an administrator.', 'Error', 'OK', 'Error')"
+    exit
 )
 
-title DAY2 /  [2%]
-ipconfig /flushdns
+:: Define the total number of steps for each routine
+set reset_network_steps=7
+set optimize_network_steps=16
 
-timeout 1 > nul
+set /a step_counter=0
 
-title DAY2 / [11%]
-netsh int ip reset
+:UpdateProgress
+:: Updates the progress percentage in the title bar
+set /a progress=(step_counter*100)/total_steps
+set title_text=DAY2 / [%progress%%]
+title %title_text%
 
+:: Logging function to write messages to a log file
+:LogMessage
+set log_file=DAY2.log
+set timestamp=%date% %time%
+echo [%timestamp%] %1 >> %log_file%
+goto :eof
 
-title DAY2 / [24%]
-ipconfig /registerdns
+:CheckError
+:: Check the error level and display a message box if an error occurred
+if %ERRORLEVEL% NEQ 0 (
+    call :LogMessage "Error occurred: %1"
+    powershell -Command "Add-Type -AssemblyName PresentationFramework; [System.Windows.MessageBox]::Show('An error occurred while executing the command: %1', 'Error', 'OK', 'Error')"
+    exit
+)
 
-
-title DAY2 / [29%]
-ipconfig /release
-
-
-title DAY2 / [34%]
-ipconfig /renew
-
-
-title DAY2 / [40%]
-netsh winsock reset
-
-
-timeout 1 > nul
-
-title DAY2 / [57%]
-TAKEOWN /F C:\Windows\Prefetch /A
-rd C:\Windows\Prefetch /s /q && md C:\Windows\Prefetch
-
-
-title DAY2 / [62%]
-TAKEOWN /F %USERPROFILE%\AppData\Local\Temp /A
-rd %USERPROFILE%\AppData\Local\Temp /s /q && md %USERPROFILE%\AppData\Local\Temp
-
-
-title DAY2 / [68%]
-TAKEOWN /F C:\Windows\Temp /A
-rd C:\Windows\Temp /s /q && md C:\Windows\Temp
-
-
-title DAY2 / [69%]
-Reg.exe add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Multimedia\SystemProfile" /v "NetworkThrottlingIndex" /t REG_SZ /d "ffffffff" /f
-
-
-title DAY2 / [80%]
-nbtstat -R
-nbtstat -RR
-
-
-title DAY2 / [92%]
-arp -d *
-
+:Menu
 cls
-title DAY2 / DONE [100%]
-echo     :::::::::      :::   :::   :::  ::::::::  
-echo     :+:    :+:   :+: :+: :+:   :+: :+:    :+: 
-echo     +:+    +:+  +:+   +:+ +:+ +:+        +:+  
-echo     +#+    +:+ +#++:++#++: +#++:       +#+    
-echo     +#+    +#+ +#+     +#+  +#+      +#+      
-echo     #+#    #+# #+#     #+#  #+#     #+#       
-echo     #########  ###     ###  ###    ########## 
-echo.     
-echo.     
-echo.     Press Any Button To Exit
-echo.     
+:: Display the main menu to the user
+echo Select an option:
+echo 1. Reset network
+echo 2. Optimize network
+echo 3. Exit
+set /p choice=Input: 
+if "%choice%" == "1" goto ResetNetwork
+if "%choice%" == "2" goto OptimizeNetwork
+if "%choice%" == "3" exit
+echo Invalid input. Please try again.
+pause
+goto Menu
 
-Pause > nul
+:ResetNetwork
+:: Set the total steps for ResetNetwork
+
+set total_steps=%reset_network_steps%
+
+set /a step_counter+=1
+call :UpdateProgress
+call :LogMessage "Executing: ipconfig /flushdns"
+ipconfig /flushdns
+call :CheckError "ipconfig /flushdns"
+
+set /a step_counter+=1
+call :UpdateProgress
+call :LogMessage "Executing: netsh int ip reset"
+netsh int ip reset
+call :CheckError "netsh int ip reset"
+
+set /a step_counter+=1
+call :UpdateProgress
+call :LogMessage "Executing: ipconfig /registerdns"
+ipconfig /registerdns
+call :CheckError "ipconfig /registerdns"
+
+set /a step_counter+=1
+call :UpdateProgress
+call :LogMessage "Executing: ipconfig /release"
+ipconfig /release
+call :CheckError "ipconfig /release"
+
+set /a step_counter+=1
+call :UpdateProgress
+call :LogMessage "Executing: ipconfig /renew"
+ipconfig /renew
+call :CheckError "ipconfig /renew"
+
+set /a step_counter+=1
+call :UpdateProgress
+call :LogMessage "Executing: netsh winsock reset"
+netsh winsock reset
+call :CheckError "netsh winsock reset"
+
+:: Exit after completing the reset
+set /a step_counter+=1
+call :UpdateProgress
+pause
+goto Menu
+
+:OptimizeNetwork
+:: Optimize network settings for better performance
+
+set total_steps=%optimize_network_steps%
+
+set /a step_counter+=1
+call :UpdateProgress
+Reg.exe add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Multimedia\SystemProfile" /v "NetworkThrottlingIndex" /t REG_SZ /d "ffffffff" /f
+call :CheckError "Reg.exe add NetworkThrottlingIndex"
+
+set /a step_counter+=1
+call :UpdateProgress
+nbtstat -R
+call :CheckError "nbtstat -R"
+
+set /a step_counter+=1
+call :UpdateProgress
+nbtstat -RR
+call :CheckError "nbtstat -RR"
+
+set /a step_counter+=1
+call :UpdateProgress
+arp -d *
+call :CheckError "arp -d *"
+
+set /a step_counter+=1
+call :UpdateProgress
+netsh interface ip delete arpcache
+call :CheckError "netsh interface ip delete arpcache"
+
+set /a step_counter+=1
+call :UpdateProgress
+netsh interface tcp set global autotuninglevel=highlyrestricted
+call :CheckError "netsh interface tcp set global autotuninglevel=highlyrestricted"
+
+set /a step_counter+=1
+call :UpdateProgress
+netsh interface tcp set global heuristics=disabled
+call :CheckError "netsh interface tcp set global heuristics=disabled"
+
+set /a step_counter+=1
+call :UpdateProgress
+netsh interface tcp set global congestionprovider=ctcp
+call :CheckError "netsh interface tcp set global congestionprovider=ctcp"
+
+set /a step_counter+=1
+call :UpdateProgress
+netsh interface tcp set global rss=enabled
+call :CheckError "netsh interface tcp set global rss=enabled"
+
+set /a step_counter+=1
+call :UpdateProgress
+netsh interface tcp set global chimney=enabled
+call :CheckError "netsh interface tcp set global chimney=enabled"
+
+set /a step_counter+=1
+call :UpdateProgress
+netsh interface tcp set global timestamps=disabled
+call :CheckError "netsh interface tcp set global timestamps=disabled"
+
+set /a step_counter+=1
+call :UpdateProgress
+netsh interface tcp set global ecncapability=enabled
+call :CheckError "netsh interface tcp set global ecncapability=enabled"
+
+set /a step_counter+=1
+call :UpdateProgress
+netsh interface tcp set global maxsynretransmissions=1
+call :CheckError "netsh interface tcp set global maxsynretransmissions=1"
+
+set /a step_counter+=1
+call :UpdateProgress
+netsh interface ip set global nonbest_effort_limit=0
+call :CheckError "netsh interface ip set global nonbest_effort_limit=0"
+
+set /a step_counter+=1
+call :UpdateProgress
+netsh winsock reset catalog
+call :CheckError "netsh winsock reset catalog"
+
+:: Exit after completing the optimization
+set /a step_counter+=1
+call :UpdateProgress
+pause
+goto Menu
